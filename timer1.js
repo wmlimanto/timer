c6e70986b092bb35c8013146f5083538529acec9
Winston Limanto <wmlimanto@gmail.com>
2022-04-14 18:37:54 -0700

    implemented timer function with base case only

---
+++ timer1.js
@@ -0,0 +1,12 @@
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+
+//takes in the command line input, removes the first two elements (node timer1.js), and sorts it
+const timer = process.argv.slice(2).sort();
+
+//executes the function (ring alarm) once for each element in the array
+timer.forEach(time => {
+  setTimeout(() => {
+    process.stdout.write('\x07')
+  }, time * 1000);
+});
+
